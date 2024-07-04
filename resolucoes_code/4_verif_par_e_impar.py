f91cd1eb1f4a59c31abd59b9deb76c8be362841e
Joanna Sabrina Villeroy <116309057+joannavilleroy@users.noreply.github.com>
2024-07-04 16:50:51 +0000

    Resolução 4 - Ímpar ou par

--- resolucoes_code/4_verif_par_e_impar.py
+++ resolucoes_code/4_verif_par_e_impar.py
@@ -1,1 +1,8 @@
 # Como entrada, receba um número inteiro e verifique se ele é par ou ímpar. Uma dica é: Utilize condicionais para realizar a verificação e, se possível, faça uso do Github Copilot(ou outra IA) para otimizar a estrutura do código.
+
+num = int(input("Digite um número: "))
+
+if num % 2 == 0:
+    print("O número {} é par".format(num))
+else:
+    print("O número {} é ímpar".format(num))
