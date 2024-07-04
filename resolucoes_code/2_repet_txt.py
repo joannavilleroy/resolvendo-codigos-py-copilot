3f0679486330c6ec205832dcaadfadcda9d6533f
Joanna Sabrina Villeroy <116309057+joannavilleroy@users.noreply.github.com>
2024-07-04 16:08:01 +0000

    Resolução 2 - Repetindo texto

--- resolucoes_code/2_repet_txt.py
+++ resolucoes_code/2_repet_txt.py
@@ -1,1 +1,6 @@
-# Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+# Agora vamos solicitar uma string e um número inteiro como entrada. Depois teremos que retornar a string repetida o número de vezes informado.
+
+texto = input ("Digite um texto: ")
+quantidade = int(input("Quantas vezes iremos repeti-lo? "))
+
+print ((texto + " ") * quantidade)
