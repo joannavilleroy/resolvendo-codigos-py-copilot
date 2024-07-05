994bd5f61f0c44404e2406d2f97a9f74e905bff2
Joanna Sabrina Villeroy <116309057+joannavilleroy@users.noreply.github.com>
2024-07-05 01:38:07 +0000

    Resolução 5 - Calculando a média

--- resolucoes_code/5_calc_media.py
+++ resolucoes_code/5_calc_media.py
@@ -1,1 +1,14 @@
 # Agora vamos calcular a média de três notas fornecidas na entrada do usuário. Uma dica é: Utilize operadores aritméticos para realizar o cálculo da média.
+
+nota1 = float(input("Insira a primeira nota: "))
+nota2 = float(input("Insira a segunda nota: "))
+nota3 = float(input("Insira a terceira nota: "))
+
+media = (nota1+nota2+nota3)/3
+
+if media < 5:
+    print("Sua média foi {:.2f}, está REPROVADO".format(media))
+elif media < 9.9:
+    print("Sua média foi {:.2f}, está APROVADO".format(media))
+else:
+    print("PARABÉNS! Sua média foi {:.2f}, está APROVADO".format(media))
