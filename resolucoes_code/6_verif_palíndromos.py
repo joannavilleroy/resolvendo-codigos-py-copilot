507a57ce225b575056316cacdf864df93106ddc5
Joanna Sabrina Villeroy <116309057+joannavilleroy@users.noreply.github.com>
2024-07-05 01:47:15 +0000

    Resolução 6 - Palíndromos

--- resolucoes_code/6_verif_palíndromos.py
+++ resolucoes_code/6_verif_palíndromos.py
@@ -1,1 +1,10 @@
 # Vamos testar se uma palavra é um palíndromo?! Uma dica é: Utilize conceitos de manipulação de strings para inverter a palavra e comparar com a original.
+
+palavra = input("Vamos testar se uma palavra é um palíndromo?! Digite a palavra para verificarmos: ")
+
+invertida = palavra[::-1]
+
+if palavra == invertida:
+    print("Essa palavra é um palíndromo")
+else:
+    print("Essa palavra não é um palíndromo, invertida ela fica assim '{}'".format(invertida))
